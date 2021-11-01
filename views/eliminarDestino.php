40ba924f118716cf01ed79f79106e70a012b274a
Guillermo <guillef33@gmail.com>
2021-11-01 11:59:41 -0300

    eliminar destino

---
+++ views/eliminarDestino.php
@@ -0,0 +1,37 @@
+<?php
+$title = 'Eliminar region';
+include('../models/destinosModels.php');
+include('../controllers/destinosController.php');
+include('modules/header.php');
+$Destino = new DestinosModel;
+$chequeo = $Destino->eliminarDestino();
+
+$css = 'danger';
+$mensaje = 'No se pudo eliminar el destino';
+if ($chequeo) {
+    $css = 'success';
+    $mensaje = 'Destino ' . $Destino->getDestNombre() . ' eliminada correctamente';
+}
+
+?>
+
+<main class="container">
+
+    <h1>Baja de un destino</h1>
+
+    <?php
+    //  regionesController::vistaEliminarRegion();
+    ?>
+
+    <div class="alert alert-<?= $css ?>">
+        <?= $mensaje ?>
+        <a href="destinosView.php" class="btn btn-light ml-2">
+            volver a panel
+        </a>
+    </div>
+
+</main>
+
+<?php
+include('modules/footer.php')
+?>
